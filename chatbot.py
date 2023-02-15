bcb1dcee0e4742fe4b1baa8a91de1aacd4dc066c
liesgame <120369068@qq.com>
2023-02-15 11:55:15 +0800

    lab4

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,26 @@
-import telegram
-from telegram.ext import Updater,MessageHandler,Filters
+from telegram import Update
+from telegram.ext import Updater,MessageHandler,Filters, CommandHandler, CallbackContext
 import configparser
 import logging
-config = configparser.ConfigParser()
-config.read('config.ini')
+import redis
 def main():
     #Load your token and create an Updater for your Bot
     config=configparser.ConfigParser()
     config.read('config.ini')
     updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context= True)
     dispatcher=updater.dispatcher
+    
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    
     #You can set this logging module,so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',level=logging.INFO)
     #register a dispatcher to handle message: here we register an echo dispatcher   
     echo_handler=MessageHandler(Filters.text&(~Filters.command),echo)
     dispatcher.add_handler(echo_handler)
+    
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
     
     # TO start the bot
     updater.start_polling()
@@ -24,5 +30,17 @@ def echo(update, context):
     logging.info("Update: "+str(update))
     logging.info("Context: "+str(context))
     context.bot.send_message(chat_id = update.effective_chat.id, text = reply_message)
+def add(update: Update, context: CallbackContext) -> None:
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]
+        # incr 自增 1
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg + 'for ' + redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage:/ add <keyword>')
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Helping you helping you')
 if __name__ == '__main__':
   main()
