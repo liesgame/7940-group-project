5b7df10dc7318e344240a57b697dded7fe50c3c1
liesgame <120369068@qq.com>
2023-03-06 00:09:24 +0800

    lab5 change config to os

--- chatbot.py
+++ chatbot.py
@@ -3,15 +3,17 @@ from telegram.ext import Updater,MessageHandler,Filters, CommandHandler, Callbac
 import configparser
 import logging
 import redis
+import os
 def main():
     #Load your token and create an Updater for your Bot
     config=configparser.ConfigParser()
-    config.read('config.ini')
-    updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context= True)
+    # config.read('config.ini')
+    # updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context= True)
+    updater=Updater(token=(os.environ['ACCESS_TOKEN']),use_context= True)
     dispatcher=updater.dispatcher
     
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
     
     #You can set this logging module,so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',level=logging.INFO)
